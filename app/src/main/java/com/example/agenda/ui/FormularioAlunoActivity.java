52f232e02b8e3bd9046c39a51dd9480fb0753934
andrebrandaoca <andrebrandaoca@gmail.com>
2020-04-20 16:41:08 -0400

    Lista de Alunos activities created

---
+++ app/src/main/java/com/example/agenda/ui/FormularioAlunoActivity.java
@@ -0,0 +1,14 @@
+package com.example.agenda.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
