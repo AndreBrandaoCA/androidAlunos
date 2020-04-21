8d8719e3b09939270f88eb5e32888677a00102bb
andrebrandaoca <andrebrandaoca@gmail.com>
2020-04-21 00:36:11 -0400

    Aluno form created

--- app/src/main/java/com/example/agenda/ui/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/FormularioAlunoActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.agenda.DAO.AlunoDAO;
 import com.example.agenda.R;
+import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -15,12 +18,24 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
 
+        AlunoDao dao = new AlunoDAO();
+
+
+        final EditText campoNome = findViewById(R.id.activity_formulatio_aluno_nome);
+        final EditText campoTelefeone = findViewById(R.id.activity_formulatio_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulatio_aluno_email);
 
         Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FormularioAlunoActivity.this, "Click no botao",Toast.LENGTH_SHORT).show();
+                String nome= campoNome.getText().toString();
+                String telefone = campoTelefeone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+                dao.salva(alunoCriado);
+
             }
         });
     }
--- app/src/main/java/com/example/agenda/ui/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/ListaAlunosActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.agenda.DAO.AlunoDAO;
 import com.example.agenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import com.example.agenda.DAO.AlunoDAO;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -17,9 +19,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+        AlunoDao dao = new AlunoDAO();
+
         setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Andre","Fran","Jose","Maria","Ana"));
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,alunos));
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,dao.todos();));
     }
 }
