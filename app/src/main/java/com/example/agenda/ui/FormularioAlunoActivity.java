6e91a6092a6a0bb16ccebe938405209cf1d1ac55
andrebrandaoca <andrebrandaoca@gmail.com>
2020-04-20 23:50:59 -0400

    Aluno form created

--- app/src/main/java/com/example/agenda/ui/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/FormularioAlunoActivity.java
@@ -2,6 +2,10 @@ package com.example.agenda.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.example.agenda.R;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
@@ -10,5 +14,14 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioAlunoActivity.this, "Click no botao",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
